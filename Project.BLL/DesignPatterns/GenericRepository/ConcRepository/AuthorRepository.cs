86236c66a52bc843242dfbe4e5f0d04f6d3c6720
cihan dilibal <dilibalcihan@gmail.com>
2024-02-21 13:46:24 +0300

    repository de degisiklik yapildi.

--- Project.BLL/DesignPatterns/GenericRepository/ConcRepository/AuthorRepository.cs
+++ Project.BLL/DesignPatterns/GenericRepository/ConcRepository/AuthorRepository.cs
@@ -3,6 +3,7 @@ using Project.Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,23 @@ namespace Project.BLL.DesignPatterns.GenericRepository.ConcRepository
         /// <summary>
         /// Bu metot Author'i eklerken beraberinde o Author'a ait özel iki makale daha ekler...
         /// </summary>
-        public void SpecialAdd()
+        public void SpecialAdd(Author item)
         {
+            Article ar = new Article()
+            {
+                Title = "Test"
+
+            };
+            Article ar2= new Article()
+            {
+                Title = "Test2"
+
+            };
+
+            item.Articles.Add(ar);
+            item.Articles.Add(ar2);
+            _db.Authors.Add(item);
+            Save();
 
             
 
